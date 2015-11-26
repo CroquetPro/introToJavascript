5601f218c29c67a877720ea590a672dd866b5ebd
Ben Rothman <croquetpro@gmail.com>
2015-11-26 14:21:32 -0800

    myEach and myMap

--- my_enumerables.js
+++ my_enumerables.js
@@ -4,4 +4,19 @@ Array.prototype.myEach = function(blk){
   }
   return this;
 };
-[1,2,3,4].myEach(console.log);
+// [1,2,3,4].myEach(console.log);
+
+
+Array.prototype.myMap = function(blk){
+  var mapped = [];
+  this.myEach(function(elem){
+    mapped.push(blk(elem));
+  });
+
+  return mapped;
+};
+var arr = [1,2,3,4];
+arr.myMap(function (el) {
+    console.log(el);
+});
+console.log(arr);
